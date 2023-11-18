d0e5e479fe4196a13891381e224a779f5eb018b2
Ayo <heyayomideadebisi@gmail.com>
2023-11-18 23:45:19 +0100

    start building create post page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import PostDetail from "./pages/PostDetail";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/create" element={<CreatePost />} />
         <Route path="/posts/post/:id" element={<PostDetail />} />
       </Routes>
     </>
---
+++ client/src/pages/CreatePost.jsx
@@ -0,0 +1,42 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import { MdCancel } from "react-icons/md";
+
+export default function CreatePost() {
+  return (
+    <>
+    <Header/>
+    <div className="createpost">
+        <div className="createpost-container">
+            <h2>Create a post</h2>
+
+            <div className="createpost-details">
+                <input type="text" placeholder="Enter your title" className="title-input" />
+                <input type="file" name="image"  />
+
+                <div className="create-category">
+                    <input type="text" placeholder="Enter your category" className="category-input" />
+                    <button className="category-button">Add</button>
+                </div>
+
+                <div className="create-categories">
+                    <div className="category">
+                        <p>Tech</p>
+                        <MdCancel className="category-icon" />
+                    </div>
+                    <div className="category">
+                        <p>Tech</p>
+                        <MdCancel className="category-icon" />
+                    </div>
+                </div>
+
+                <textarea name="description" cols="30" rows="10" placeholder="Enter your description" className="description-input"></textarea>
+                <button className="create-button">Create</button>
+            </div>
+        </div>
+    </div>
+
+    <Footer/>
+    </>
+  )
+}
