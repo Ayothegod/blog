77a1be20069f43796e427ca10c20fbab1a414595
Ayo <heyayomideadebisi@gmail.com>
2023-11-18 19:39:16 +0100

    build register ui

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,20 @@
+import { Link } from "react-router-dom"
 
 export default function Register() {
   return (
-    <div>Register</div>
+    <div className="login">
+    <div className="login-container">
+        <h1 className="login-title">Create an account</h1>
+        <input className="email-input" type="text" placeholder="enter your username" />
+        <input className="email-input" type="text" placeholder="enter your email" />
+        <input className="email-input" type="password" placeholder="enter your password" />
+        <button className="login-button">Register</button>
+
+        <div className="login-doubt">
+            <p>Already have an account?</p>
+            <Link to="/login" className="login-register">Login</Link>
+        </div>
+    </div>
+</div>
   )
 }
