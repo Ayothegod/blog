987fe437daaaaa6e0dc8015219a658437ae52ef5
Ayo <heyayomideadebisi@gmail.com>
2023-11-18 11:19:10 +0100

    add assets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./pages/Home";
 
 function App() {
-
   return (
     <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,3 +1,39 @@
+import { Link } from "react-router-dom";
+import { CiSearch } from "react-icons/ci";
+
 export default function Header() {
-  return <div>Header</div>;
+  const user = "Ayomide";
+
+  return (
+    <div className="header">
+      <h1 className="header-logo">Blog Market</h1>
+
+      <div className="header-search">
+        <CiSearch className="search-icon" />
+        <input type="text" placeholder="search a post" className="search-box" />
+      </div>
+
+      <div className="header-auth">
+        {!user ? (
+          <>
+            <h3 className="auth auth-login">
+              <Link to="/login">Login</Link>
+            </h3>
+            <h3 className="auth auth-login">
+              <Link to="/register">Register</Link>
+            </h3>
+          </>
+        ) : (
+          <>
+            <h3 className="auth auth-create">
+              <Link to="/create">Create</Link>
+            </h3>
+            <h3 className="auth auth-profile">
+              <Link to="/profile">Profile</Link>
+            </h3>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
