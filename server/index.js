1c3e38ee0b047df182ea04f53c49f62847e49178
Ayo <heyayomideadebisi@gmail.com>
2023-12-04 13:47:30 +0100

    feat: add auth route

--- server/index.js
+++ server/index.js
@@ -9,18 +9,18 @@ const { createUser } = require("./libs/db");
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-const startDb = async () => {
-  await mongoose.connect( process.env.MONGO_URL );
-};
 
 app.get("/", (req, res) => {
   res.status(200).send("hello backend coders");
 });
-app.use("/api", createUser);
 
-startDb()
-  .then(console.log("database is connected!"))
-  .catch((e) => console.log(e));
+// const startDb = async () => {
+//   await mongoose.connect( process.env.MONGO_URL );
+// };
+
+// startDb()
+//   .then(console.log("database is connected!"))
+//   .catch((e) => console.log(e));
   
 app.listen(port, () => console.log(`server listening on port: ${port}`));
 
